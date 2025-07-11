be227489e52ccc6e803ccf3630d507671007f708
study or sadness. <workhardforfuture4177@gmail.com>
2025-07-11 17:56:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ide-media-prod.a.run.app',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
